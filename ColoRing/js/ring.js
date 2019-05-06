4be8eafd464e001f585b69f37235cc6ffed89579
bcatoto <biancacatoto999@gmail.com>
2019-05-06 13:41:58 -0400

    update Arena

---
+++ ColoRing/js/ring.js
@@ -0,0 +1,6 @@
+function createArena() {
+  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  var cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+}
