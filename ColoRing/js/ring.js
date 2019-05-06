53fbf07bbd12b0e6ada0e72e3b35e744660f55b1
bcatoto <biancacatoto999@gmail.com>
2019-05-06 14:16:38 -0400

    maze_bianca

--- ColoRing/js/ring.js
+++ ColoRing/js/ring.js
@@ -1,12 +1,14 @@
 function createArena() {
+  // material
+  const mat = new THREE.MeshBasicMaterial( { color: 0x878e88 } );
+
   // create tiles
   const tileGeo = new THREE.BoxGeometry( 1, 1, 0.1 );
-  const tileMat = new THREE.MeshBasicMaterial( { color: 0x878e88 } );
 
   tiles = [];
   for (let x = -70; x < 71; x++) {
     for (let y = -50; y < 51; y++) {
-      const tile = new THREE.Mesh( tileGeo, tileMat );
+      const tile = new THREE.Mesh( tileGeo, mat );
       tile.position.x = x;
       tile.position.y = y;
       tile.position.z = 0.05;
@@ -14,4 +16,12 @@ function createArena() {
       scene.add(tile);
     }
   }
+
+  // create walls
+  const wallHorGeo = new THREE.BoxGeometry( 131, 10, 10 );
+  const wallVerGeo = new THREE.BoxGeometry( 10, 131, 10 );
+
+  // top wall
+  const wallTop = new THREE.Mesh(wallHorGeo, mat);
+  scene.add(wallTop);
 }
