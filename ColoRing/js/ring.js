4265bc98c12aaf6bfbe29c38035db903c104af89
bcatoto <biancacatoto999@gmail.com>
2019-05-06 14:09:50 -0400

    updated ring.js

--- ColoRing/js/ring.js
+++ ColoRing/js/ring.js
@@ -1,7 +1,17 @@
 function createArena() {
-  // create walls
-  var geometry = new THREE.BoxGeometry( 10, 10, 10 );
-  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  var cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
+  // create tiles
+  const tileGeo = new THREE.BoxGeometry( 1, 1, 0.1 );
+  const tileMat = new THREE.MeshBasicMaterial( { color: 0x878e88 } );
+
+  tiles = [];
+  for (let x = -70; x < 71; x++) {
+    for (let y = -50; y < 51; y++) {
+      const tile = new THREE.Mesh( tileGeo, tileMat );
+      tile.position.x = x;
+      tile.position.y = y;
+      tile.position.z = 0.05;
+      tiles.push(tile);
+      scene.add(tile);
+    }
+  }
 }
