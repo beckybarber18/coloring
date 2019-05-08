47bdc58c6f5201af9fe0346f0c2243421756e9ec
Rebecca <rb25@princeton.edu>
2019-05-07 20:33:54 -0400

    unbind the arrow keys

--- ColoRing/keyboard.js
+++ ColoRing/keyboard.js
@@ -474,6 +474,13 @@
 
 	}
 
+	window.addEventListener("keydown", function(e) {
+	    // space and arrow keys
+	    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+	        e.preventDefault();
+	    }
+	}, false);
+
 	return {
 		"bind": {
 			"key": bindKey,
